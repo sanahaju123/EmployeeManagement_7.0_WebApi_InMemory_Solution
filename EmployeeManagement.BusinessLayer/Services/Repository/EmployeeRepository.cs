61b2fd4975965fabbc8f4da41087a970c42791f3
user <sanahaju123@gmail.com>
2023-07-26 16:52:47 +0530

    Add project files.

---
+++ EmployeeManagement.BusinessLayer/Services/Repository/EmployeeRepository.cs
@@ -0,0 +1,34 @@
+﻿using EmployeeManagement.DataLayer;
+using EmployeeManagement.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.BusinessLayer.Services.Repository
+{
+
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly EmployeeDbContext _dbContext;
+
+        public EmployeeRepository(EmployeeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            // Hardcoded employee details
+            var employees = new List<Employee>
+            {
+                new Employee { EmployeeID = 1, EmployeeName = "John Doe", Salary = 50000 },
+                new Employee { EmployeeID = 2, EmployeeName = "Jane Smith", Salary = 60000 },
+                // Add more employees as needed
+            };
+
+            return employees;
+        }
+    }
+}
---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagement.BusinessLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        [Route("/employees")]
+        public IActionResult GetEmployees()
+        {
+            var employees = _employeeService.GetEmployees();
+            return Ok(employees);
+        }
+    }
+}
